7ee30c16801d0250715a6bb77c40934ea317740b
Victor Redondo <dev.victor.redondo@gmail.com>
2018-03-27 17:05:38 -0600

    add initial store config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import * as nanoid from 'nanoid';
+
+import { TodoState } from './todo.state';
+import { AddTodo } from './todo.actions';
+import { Select, Store } from 'ngxs';
+import { Observable } from 'rxjs/Observable';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @Select(TodoState.todos) todos$: Observable<Todo[]>;
+
+  title = 'Todos';
+
+  constructor(private store: Store) {
+    const currentDate = new Date();
+    const todo: Todo = {
+      id: nanoid(),
+      createdDate: currentDate.toLocaleTimeString(),
+      completedDate: null,
+      isCompleted: true,
+      description: `Test ${currentDate.toLocaleTimeString()}`
+    };
+    this.store.dispatch(new AddTodo(todo)).subscribe(() => {
+      console.log('New todo added');
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxsModule, NgxsReduxDevtoolsPluginModule } from 'ngxs';
 
 import { AppComponent } from './app.component';
-
-
+import { TodoState } from './todo.state';
+import { TodoListComponent } from './todo-list.component';
+import { TodosComponent } from './containers/todos.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodoListComponent, TodosComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([TodoState]),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
